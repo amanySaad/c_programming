12fd3903677be158f4d68b1421958ee106d3adcd
amanySaad <eng.am.aboelella@gmail.com>
2023-07-14 05:31:39 -0700

    @update@

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 48 to 57.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
