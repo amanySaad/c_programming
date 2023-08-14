da4d7f32c1a9960cd3a5de3a18cca89e7db5d0cb
amanySaad <eng.am.aboelella@gmail.com>
2023-08-14 14:05:05 -0700

    fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -36,7 +36,7 @@ char *_strcpy(char *dest, char *src)
 
 	len = 0;
 
-while (src[len] != '\0')
+	while (src[len] != '\0')
 	{
 		len++;
 	}
@@ -76,7 +76,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-dog->owner = malloc(sizeof(char) * (len2 + 1));
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog);
