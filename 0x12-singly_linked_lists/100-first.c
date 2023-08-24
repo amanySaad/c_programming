6f793afae64628c7776f7e712adce03f2344c42b
amanySaad <eng.am.aboelella@gmail.com>
2023-08-24 15:29:21 -0700

    change first function name

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-void run_before(void) __attribute__ ((constructor));
+void first(void) __attribute__ ((constructor));
 
-void run_before(void)
+void first(void)
 {
  printf("You're beat! and yet, you must allow,\n");
  printf("I bore my house upon my back!\n");
